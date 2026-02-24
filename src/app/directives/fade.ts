5171bd5c232a03715ea11f21f120e2b19de81212
GeronimoGM <geronimogonzalezmartino@gmail.com>
2026-02-24 19:22:49 -0300

    fix: ssr was not working because IntersectionObserver is not defined on server side

--- src/app/directives/fade.ts
+++ src/app/directives/fade.ts
@@ -1,4 +1,5 @@
-import { Directive, ElementRef, inject, input, OnInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Directive, ElementRef, inject, input, OnInit, PLATFORM_ID } from '@angular/core';
 
 type FadeDirection = 'up' | 'left' | 'right';
 
@@ -7,39 +8,42 @@ type FadeDirection = 'up' | 'left' | 'right';
 })
 export class Fade implements OnInit {
   private readonly el = inject(ElementRef<HTMLElement>);
+  private readonly isBrowser = isPlatformBrowser(inject(PLATFORM_ID));
 
   readonly direction = input<FadeDirection>('up');
 
   ngOnInit(): void {
-    const target =
-      Array.from(this.el.nativeElement.children).find((c) => c instanceof HTMLElement) ||
-      this.el.nativeElement;
+    if (this.isBrowser) {
+      const target =
+        Array.from(this.el.nativeElement.children).find((c) => c instanceof HTMLElement) ||
+        this.el.nativeElement;
 
-    const translateMap = {
-      up: 'translate-y-6',
-      left: 'translate-x-6',
-      right: '-translate-x-6',
-    };
+      const translateMap = {
+        up: 'translate-y-6',
+        left: 'translate-x-6',
+        right: '-translate-x-6',
+      };
 
-    target.classList.add(
-      'opacity-0',
-      translateMap[this.direction()],
-      'transition-all',
-      'duration-1000',
-      'ease-out',
-    );
+      target.classList.add(
+        'opacity-0',
+        translateMap[this.direction()],
+        'transition-all',
+        'duration-1000',
+        'ease-out',
+      );
 
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          target.classList.remove('opacity-0', translateMap[this.direction()]);
-          target.classList.add('opacity-100', 'translate-x-0', 'translate-y-0');
-          observer.disconnect();
-        }
-      },
-      { threshold: 0.2 },
-    );
+      const observer = new IntersectionObserver(
+        ([entry]) => {
+          if (entry.isIntersecting) {
+            target.classList.remove('opacity-0', translateMap[this.direction()]);
+            target.classList.add('opacity-100', 'translate-x-0', 'translate-y-0');
+            observer.disconnect();
+          }
+        },
+        { threshold: 0.2 },
+      );
 
-    observer.observe(target);
+      observer.observe(target);
+    }
   }
 }
